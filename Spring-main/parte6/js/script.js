676612ab03531bf910412c7ac14434d9a0b8d6cb
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2026-02-24 11:44:46 -0300

    ATIVIDADE LOGIN - 24/02
    AAAAAAAAAAA

---
+++ Spring-main/parte6/js/script.js
@@ -0,0 +1,9 @@
+// fetch('http://localhost:8080/usuarios') 
+// .then(response => response.json())
+// .then(data => {
+//     const tableBody = document.getElementById('table-body');
+//     data.forEach (row => {
+//         tableBody.innerHTML += `<tr><td>${row.id}</td><td>${row.nome}</td>`;
+//     });
+// });
+
